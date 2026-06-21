3dc73924210e835d01c1b7194f85ad245eba278c
ohataken <11092425+ohataken@users.noreply.github.com>
2026-06-21 15:05:12 +0900

    feat: add card description association

--- spec/models/card_description_spec.rb
+++ spec/models/card_description_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe CardDescription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it 'belongs to a card' do
+      card = Card.create!(name: '打', pinyin: 'dǎ')
+      description = CardDescription.create!(card: card, content: 'to hit')
+
+      expect(description.card).to eq(card)
+    end
+  end
 end
