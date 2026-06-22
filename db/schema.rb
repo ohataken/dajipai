88272ee9d5ef0ecf39f68650002227fd03d9263e
ohataken <11092425+ohataken@users.noreply.github.com>
2026-06-23 08:58:53 +0900

    chore: remove card description unique index migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_21_054133) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_21_054132) do
   create_table "card_descriptions", force: :cascade do |t|
     t.integer "card_id", null: false
     t.string "content"
